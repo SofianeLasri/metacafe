54f7c121cb1f8d6dbf66e0cdfe19d64f36b91e01
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2024-06-24 17:00:31 +0200

    Ajout env port front

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,24 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const port = `${env.VITE_PORT ?? '5173'}`;
+
+    return {
+        resolve: {
+            alias: {
+                '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+                '~@': path.resolve(__dirname, 'src'),
+            }
+        },
+        plugins: [
+            vue(),
+        ],
+        server: {
+            port: port,
         }
-    },
-    plugins: [
-        vue(),
-    ],
+    };
 })
